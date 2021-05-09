99e8678b6883849eac435a0c8a02804a55b8d025
yoshihollez <36663359+yoshihollez@users.noreply.github.com>
2021-05-10 01:39:46 +0200

    fixed cookie not being set

--- employeehappiness_api/src/app.ts
+++ employeehappiness_api/src/app.ts
@@ -1,84 +1,97 @@
+// imports
 import "reflect-metadata";
-import { MikroORM } from "@mikro-orm/core";
-import * as dotenv from "dotenv";
-import { __prod__ } from "./constants";
-import microConfig from "./mikro-orm.config";
 import express from "express";
-import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
-import { HelloResolver } from "./resolvers/hello";
-import { PostResolver } from "./resolvers/post";
-import { EmployeeHappinessResolver } from "./resolvers/employeeHappiness";
-import redis from "redis";
-import session from "express-session";
+import cors from "cors";
+import dotenv from "dotenv";
 import connectRedis from "connect-redis";
 import { createConnection } from "typeorm";
+import { ApolloServer } from "apollo-server-express";
+import { UserResolver } from "./resolvers/User";
 import { User } from "./entities/User";
+import session from "express-session";
+import redis from "redis";
+import { HappinessResolver } from "./resolvers/Happiness";
 import { Happiness } from "./entities/Happiness";
 
+// dotenv data
 dotenv.config();
-const PORT: string = process.env.PORT || "";
+const PORT: string = process.env.PORT || "4000";
 
+// everything is placed in a main class so async can be used.
 const main = async () => {
-  const orm = await MikroORM.init(microConfig);
-  orm.getMigrator().up();
   const app: express.Application = express();
 
+  // connection for database with typeorm
   const conn = await createConnection({
     type: "mysql",
-    database: "test",
+    database: "employeehappiness",
     username: "root",
     password: "",
     url: "",
     logging: true,
-    synchronize: true, // (not in production)
-    // migrations: [path.join(__dirname, "./migrations/*")],
+    synchronize: true, // automatically updates the database tables, disable in production.
+    // migrations: [path.join(__dirname, "./migrations/*")], // for manual migrations
     entities: [User, Happiness],
   });
-
+  // redis setup, used to store sessiondata
   const RedisStore = connectRedis(session);
   const redisClient = redis.createClient(6373);
 
+  var routesArray = ["/login", "/rating", "/statistics", "/graphql"];
+
+  // cookie settings
   app.use(
+    routesArray,
     session({
       name: "qid",
-      store: new RedisStore({ client: redisClient, disableTouch: true }),
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true,
+      }),
       secret: "afefgegzgzergdfghtyrjykiluimomrfsdvb",
       resave: false,
       saveUninitialized: false,
       cookie: {
+        path: "/",
         maxAge: 1000 * 60 * 30, //30 min
         httpOnly: true,
         sameSite: "lax",
-        secure: __prod__, // only in https
+        secure: false, // only in https, enable in production.
       },
     })
   );
-
+  // cors setup
+  app.use(
+    cors({
+      origin: "http://localhost:3000/",
+      credentials: true,
+      preflightContinue: true,
+    })
+  );
+  // apollo setup
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver, PostResolver, EmployeeHappinessResolver],
+      resolvers: [UserResolver, HappinessResolver],
       validate: false,
     }),
     context: ({ req, res }) => ({
-      em: orm.em,
       req,
       res,
     }),
   });
-  apolloServer.applyMiddleware({ app });
-  // const post = orm.em.create(Post, { title: "blabla" });
-  // orm.em.persistAndFlush(post);
-  // // Just checking if given PORT variable is an integer or not
-  let port: number = parseInt(PORT || "");
-  if (isNaN(port) || port === 0) {
-    port = 4000;
-  }
 
-  app.listen(port, "0.0.0.0", () => {
-    console.log(`Server Started at PORT: ${port}  `);
+  apolloServer.applyMiddleware({
+    app,
+    // if you put / at the end of 3000 the cookie wil not be set.
+    cors: { origin: "http://localhost:3000" },
   });
-  // const posts = await orm.em.find(Post, {});
-  // console.log(posts);
+
+  // sets up server with port from dotenv or default 4000
+  app.listen(parseInt(PORT), "0.0.0.0", () => {
+    console.log(`🚀 Server Started at PORT: ${PORT}  `);
+  });
 };
+
+// calls main function
 main();
